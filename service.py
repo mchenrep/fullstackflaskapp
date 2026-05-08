5b7c28fd426daf44c17bb8704efdb4e53c98b956
mchenrep <matthewchenwork@gmail.com>
2026-05-07 19:46:42 -0500

    debugged, added concurrency and multithread lock

--- service.py
+++ service.py
@@ -1,63 +1,99 @@
 import sqlite3
 from socket import *
-from threading import Thread
-import datetime
-import sys
+from threading import Thread, Lock
+from datetime import datetime
+from queue import Queue
 
-def connect():
-    '''
-        Helper function to connect to 'bank.db' database
-        - Returns cursor
-    '''
-    connection = sqlite3.connect('bank.db')
-    cursor = connection.cursor()
-    return connection, cursor
+class TransactionServer:
+    def __init__(self, worker_count=4):
+        # define workers and initialize queue
+        self.worker_count = worker_count
+        self.task_queue = Queue()
+        self.workers = []
+        self.lock = Lock()
 
-def handle_transaction(from_account, to_account, amount):
-    '''
-        Function to handle transactions
-        - Logs them into database under 'transactions' table, then updates to_account and from_account
-    '''
-    connection, cursor = connect()
+    def start(self):
+        # start threads
+        for _ in range(self.worker_count):
+            thread = Thread(target=self.loop, daemon=True)
+            thread.start()
+            self.workers.append(thread)
 
-    # Log transaction
-    data = [from_account, to_account, amount, datetime.now()]
-    cursor.execute('''
-        INSERT INTO transactions (from_account, to_account, amount, timestamp) VALUES (?,?,?,?)
-    ''', data)
+    def submit_task(self, from_account, to_account, amount):
+        task = {
+            "from" : from_account,
+            "to" : to_account,
+            "amount": amount
+        }
+        self.task_queue.put(task)
 
-    # Update to and from accounts
-    # From Account
-    cursor.execute('''
-        SELECT balance
-        FROM accounts 
-        WHERE id = ?    
-    ''', (from_account,))
-    from_balance = cursor.fetchone()
-    
-    cursor.execute('''
-        UPDATE accounts 
-        SET balance = ?
-        WHERE id = ?    
-    ''', (from_balance-amount, from_account)) 
-
-    # To Account
-    cursor.execute('''
-        SELECT balance
-        FROM accounts 
-        WHERE id = ?    
-    ''', (to_account,))
-    to_balance = cursor.fetchone()
-    
-    cursor.execute('''
-        UPDATE accounts 
-        SET balance = ?
-        WHERE id = ?    
-    ''', (to_balance+amount, to_account)) 
+    def loop(self):
+        while True:
+            task = self.task_queue.get()
+            self.handle_transaction(task)
 
 
-    # Commit and close
-    connection.commit()
-    cursor.close()
-    connection.close()
+    # ------------------------------------------- SQLite ------------------------------------------------------
 
+    def connect(self):
+        '''
+            Helper function to connect to 'bank.db' database
+            - Returns cursor
+        '''
+        connection = sqlite3.connect('bank.db')
+        cursor = connection.cursor()
+        return connection, cursor
+
+    def handle_transaction(self, task):
+        '''
+            Function to handle transactions
+            - Logs them into database under 'transactions' table, then updates to_account and from_account
+        '''
+        connection, cursor = self.connect()
+        from_account, to_account, amount = task["from"], task["to"], task["amount"]
+        
+        # Log transaction
+        data = [from_account, to_account, amount, datetime.now()]
+        with self.lock:
+            cursor.execute('''
+                INSERT INTO transactions (from_account, to_account, amount, timestamp) VALUES (?,?,?,?)
+            ''', data)
+
+        # Update to and from accounts
+        
+        # From Account
+        with self.lock:
+            cursor.execute('''
+                SELECT balance
+                FROM accounts 
+                WHERE id = ?    
+            ''', (from_account,))
+        from_balance = cursor.fetchone()[0] # fetchone returns a tuple
+        
+        with self.lock:
+            cursor.execute('''
+                UPDATE accounts 
+                SET balance = ?
+                WHERE id = ?    
+            ''', (from_balance-amount, from_account)) 
+
+        # To Account
+        with self.lock:
+            cursor.execute('''
+                SELECT balance
+                FROM accounts 
+                WHERE id = ?    
+            ''', (to_account,))
+        to_balance = cursor.fetchone()
+        
+        with self.lock:
+            cursor.execute('''
+                UPDATE accounts 
+                SET balance = ?
+                WHERE id = ?    
+            ''', (to_balance+amount, to_account)) 
+
+        # Commit and close
+        connection.commit()
+        cursor.close()
+        connection.close()
