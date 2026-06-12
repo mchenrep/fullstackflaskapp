88a2ac8bd08f1ae09c8e69f458fb6b57ed6b2ec8
mchenrep <matthewchenwork@gmail.com>
2026-06-11 19:58:24 -0500

    migrated schema.py to postgres

--- schema.py
+++ schema.py
@@ -1,8 +1,22 @@
-import sqlite3
+import os
 import psycopg2
 
-# Connect to 'bank.db' database
-connection = sqlite3.connect('bank.db')
+# Connect to postgres database
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+
+
+connection = psycopg2.connect(
+    dbname = DB_NAME,
+    user = DB_USER,
+    password = DB_PASSWORD,
+    host = DB_HOST,
+    port = DB_PORT
+)
+
 cursor = connection.cursor()
 
 # Drop tables if they exist
